0847ee8b4bc5e8e04f8ecdb049d05d2676ccd6f6
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-08 16:26:27 +0600

    adding a toggle function for button

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import CreateOrder from "./CreateOrder";
 import OrderItems from "./OrderItems";
-import OrderSummary from './OrderSummary'
+import OrderSummary from './OrderSummary';
 import OrderReportSection from "./OrderReportSection";
 
 function DashBoard() {
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const handleSelectItem = (itemId) => {
+   setSelectedItems((prevSelected) => 
+    prevSelected.includes(itemId)
+    ? prevSelected.filter((id) => id !== itemId)
+    : [...prevSelected, itemId]
+  )
+  };
+
   return (
     <div className="flex flex-col md:grid md:grid-cols-3 md:grid-rows-3 gap-4 h-auto md:h-[100vh] p-3">
-      {/* Left Section: Create Order & Order Items */}
+      {/* Left Section */}
       <div className="bg-[#353333] rounded px-3 py-5 md:row-start-1 md:row-end-[-1] md:col-span-1">
         <CreateOrder />
-        <OrderItems />
+        <OrderItems
+          selectedItems={selectedItems}
+          onSelectItem={handleSelectItem}
+        />
       </div>
 
-      {/* Right Section: Order Summary & Report */}
+      {/* Right Section */}
       <div className="rounded flex flex-col gap-4 md:col-span-2">
-        <OrderSummary />
+        <OrderSummary selectedItems={selectedItems} />
         <OrderReportSection />
       </div>
     </div>
