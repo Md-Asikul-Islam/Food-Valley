7998fa98634b3cca5885308c84895caa50a37837
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-08 10:19:24 +0600

    adding a array of object in data js for listing

---
+++ src/data.js
@@ -0,0 +1,33 @@
+// public/data/foodItems.js
+export const foodItems = [
+  {
+    id: 1,
+    name: "Hamburger",
+    price: 300,
+    image: "/assets/hamburger.svg",
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    price: 450,
+    image: "/assets/pizza.svg",
+  },
+  {
+    id: 3,
+    name: "chicken",
+    price: 150,
+    image: "/assets/chicken.svg",
+  },
+    {
+    id: 4,
+    name: "pasta",
+    price: 210,
+    image: "/assets/pasta.png",
+  },
+    {
+    id: 5,
+    name: "chicken",
+    price: 180,
+    image: "/assets/soup.svg",
+  },
+];
