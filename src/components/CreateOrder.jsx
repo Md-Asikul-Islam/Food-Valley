cf3d372b069c4875ba5855633c750d5f1a8c756c
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-08 10:25:15 +0600

    adding some styles for create order

--- src/components/CreateOrder.jsx
+++ src/components/CreateOrder.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
-function CreateOrder() {
+import React from "react";
+const CreateOrder = () => {
   return (
-    <div>CreateOrder</div>
-  )
-}
-
-export default CreateOrder
+    <>
+      <h2 className="text-xl font-bold mb-1 text-white">CREATE ORDER</h2>
+      <p className="text-gray-400 text-sm mb-4 py-1">
+        Accurately fulfill customer orders based on a precise understanding of
+        their requirements.
+      </p>
+      <div className="mb-4">
+        <label className="block text-white font-medium mb-2">
+          Customer Name
+        </label>
+        <input
+          type="text"
+          className="w-full bg-gray-700 bg-opacity-50 rounded-md text-white focus:outline-none focus:ring-1 focus:ring-blue-500 px-4 py-2 transition-all duration-300"
+        />
+      </div>
+    </>
+  );
+};
+export default CreateOrder;
