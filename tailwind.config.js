cb3d9490dad21c0eba5dea307eead7e478019548
Md-Asikul-Islam <mdasikuli48@gmail.com>
2025-07-08 10:58:51 +0600

    adding a scroll bar from tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+import plugin from 'tailwind-scrollbar'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    plugin,
+  ],
+}
